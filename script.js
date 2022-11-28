ca1213b57d8254c6d572a2d676f6c9b1e4b1e410
XimenaVelasco <12ximenavelasco@gmail.com>
2022-11-28 18:38:15 -0500

    add commit and README.md

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
-      display.innerText = "";
+      display.innerText = ""; //cadena de texto vacia
     } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
+      let string = display.innerText.toString();//convertir en texto toString ...
+      display.innerText = string.substr(0, string.length - 1);//(0, el primer valor)borrar 1 a 1 
     } else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
     } else if (display.innerText == "" && item.id == "equal") {
@@ -19,12 +19,13 @@ buttons.forEach((item) => {
   };
 });
 
-const themeToggleBtn = document.querySelector(".container");
-const calculator = document.querySelector(".calculator-dark");
-const toggleIcon = document.querySelector(".calculator");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+//foreach aqui se manipulan los arrays, ¡que son los array y los objects? IMPORTANTE
+//objects. evento (e) la forma en que el usuario
+//manipula la pagina 
+//por cada boton haga esto ... 
+//buscar que son los operadores y como funcionan en js 
+// si el usuario da click y ... (operadores)
+//eval no es buena practica 
+//que es setTimeOut 
+//2000 ms = 2s
+// que es + = 
